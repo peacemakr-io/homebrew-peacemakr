762593c49fd59659d1390f9f47547c58d8985f74
Daniel Huang <yhung119@illinois.edu>
2020-03-08 15:59:27 +0800

    use macos exec

--- Formula/peacemakr-cli.rb
+++ Formula/peacemakr-cli.rb
@@ -4,13 +4,13 @@
 class PeacemakrCli < Formula
   desc "CLI for Peacemakr's Secure Data Platform (Dockerhub: https://hub.docker.com/r/peacemakr/peacemakr-cli )"
   homepage "https://peacemakr.io"
-  url "https://github.com/peacemakr-io/peacemakr-cli/releases/download/v0.1.0/peacemakr-cli"
-  sha256 "029a65d7fedc723ecf99257aaf240457df7e4375270ba85f0752169893aa846e"
+  url "https://github.com/peacemakr-io/peacemakr-cli/releases/download/v0.1.0/peacemakr-cli-macos"
+  sha256 "5a548fc8d8a9367a1c5c1172a7d1313066431515f05d671d16e769af2bf16b26"
 
   # depends_on "cmake" => :build
 
   def install
-  	bin.install "peacemakr-cli"
+  	bin.install "peacemakr-cli-macos"
   end
 
 end
