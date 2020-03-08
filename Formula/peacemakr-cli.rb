a75563476ac6d52a5d2b292d470a7decb36cdd1f
Daniel Huang <danielhuang37@outlook.com>
2020-03-08 16:23:02 +0800

    Update to download from tar

--- Formula/peacemakr-cli.rb
+++ Formula/peacemakr-cli.rb
@@ -4,8 +4,8 @@
 class PeacemakrCli < Formula
   desc "CLI for Peacemakr's Secure Data Platform (Dockerhub: https://hub.docker.com/r/peacemakr/peacemakr-cli )"
   homepage "https://peacemakr.io"
-  url "https://github.com/peacemakr-io/peacemakr-cli/releases/download/v0.1.0/peacemakr-cli-macos"
-  sha256 "5a548fc8d8a9367a1c5c1172a7d1313066431515f05d671d16e769af2bf16b26"
+  url "https://github.com/peacemakr-io/peacemakr-cli/releases/download/v0.1.0/peacemakr-cli_v10.1.0-Darwin_amd64.tar.gz"
+  sha256 "2100b8975535d207b92ddbc2e805cc4d49e627125a9f4b17060b8941375ea1ab"
 
   # depends_on "cmake" => :build
 
