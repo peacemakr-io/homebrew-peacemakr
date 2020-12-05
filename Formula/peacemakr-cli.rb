7950b708d1bdd349b9438387ab0b571700843085
Daniel Huang <yhung119@illinois.edu>
2020-12-06 07:55:31 +0800

    fixup sha256

--- Formula/peacemakr-cli.rb
+++ Formula/peacemakr-cli.rb
@@ -5,11 +5,11 @@ class PeacemakrCli < Formula
   desc "CLI for Peacemakr's Secure Data Platform (Dockerhub: https://hub.docker.com/r/peacemakr/peacemakr-cli )"
   homepage "https://peacemakr.io"
   url "https://github.com/peacemakr-io/peacemakr-cli/releases/download/v0.2.0/peacemakr-cli_v0.2.0-Darwin_amd64.tar.gz"
-  sha256 "2100b8975535d207b92ddbc2e805cc4d49e627125a9f4b17060b8941375ea1ab"
+  sha256 "562c04072506743ae7b5103d4f75fad53360bdb8fb4500f82907dcf2742f208c"
 
   resource "peacemakr-core-crypto" do
     url "https://github.com/peacemakr-io/peacemakr-core-crypto/releases/download/v0.2.0/peacemakr-core-crypto-macos-x86_64.tar.gz"
-    sha256 "562c04072506743ae7b5103d4f75fad53360bdb8fb4500f82907dcf2742f208c"
+    sha256 "9feabed563896817a5ccc612312724151c24f9978ba4e6ca8a2fc0cd9d47e082"
   end
 
   def install
