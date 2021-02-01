b5c850adf6f1a5e66aa4d563e4e46b534eae05cc
Daniel Huang <danielhuang37@outlook.com>
2021-02-01 00:04:59 -0800

    Upgrade peacemakr-cli and coreCrypto
    Upgrade peacemakr-cli to v0.3.0
    Upgrade coreCrypto to v0.2.2

--- Formula/peacemakr-cli.rb
+++ Formula/peacemakr-cli.rb
@@ -4,17 +4,16 @@
 class PeacemakrCli < Formula
   desc "CLI for Peacemakr's Secure Data Platform (Dockerhub: https://hub.docker.com/r/peacemakr/peacemakr-cli )"
   homepage "https://peacemakr.io"
-  url "https://github.com/peacemakr-io/peacemakr-cli/releases/download/v0.2.3/peacemakr-cli-Darwin_amd64.tar.gz"
-  sha256 "92ed63258684bb51227fc9336127df186d102c1c244b2fdbcad16bd0a81310bc"
+  url "https://github.com/peacemakr-io/peacemakr-cli/releases/download/v0.3.0/peacemakr-cli-Darwin_amd64.tar.gz"
+  sha256 "8542785ef7859182884044eecafb618e9695ab7da0a4621674e3effb328ee09c"
 
   resource "peacemakr-core-crypto" do
-    url "https://github.com/peacemakr-io/peacemakr-core-crypto/releases/download/v0.2.1-beta/peacemakr-core-crypto-macos-x86_64.tar.gz"
-    sha256 "fcb9c361b01484a6404941524c863a30ec3db752cc420178ff80e6ccd8c9cc45"
+    url "https://github.com/peacemakr-io/peacemakr-core-crypto/releases/download/v0.2.2/peacemakr-core-crypto-macos-x86_64.tar.gz"
+    sha256 "af161ea41b38999713fe2342073d7c0fe9e5499ce82eedd2a92e2b7a67c94b52"
   end
 
   def install
     resource("peacemakr-core-crypto").stage do
-      lib.install "lib/libpeacemakr-core-crypto-cpp.dylib"
       lib.install "lib/libpeacemakr-core-crypto.dylib"
     end
   	bin.install "peacemakr-cli"
